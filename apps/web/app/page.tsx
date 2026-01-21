a0fd27156ff03ff62a95997b56e3bc4bc9c89a89
rlebhar-ext <rlebhar-ext@klemo.com>
2026-01-21 15:34:03 +0100

    tailwind todo

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,11 @@
-import { Button } from '@repo/ui/button';
+import { Button } from '@repo/ui/button/button';
 
 export default async function Home() {
   return (
     <div >
       <main >
         <h1 className='text-2xl font-bold'>Hello World</h1>
-        <Button>Click me</Button>
+        <Button>UI package button</Button>
       </main>
 
       <footer >
