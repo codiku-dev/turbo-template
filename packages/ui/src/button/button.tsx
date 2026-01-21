9c32a1182344bbbba24296eedc1ecf803279ade6
rlebhar-ext <rlebhar-ext@klemo.com>
2026-01-21 16:42:49 +0100

    Enhance development and build scripts in package.json and turbo.json
    - Added new scripts for concurrent development of web, styles, and components.
    - Introduced build and development configurations for styles and components in turbo.json.
    - Updated globals.css to import styles from the UI package.
    - Modified button component's background color for improved UI consistency.

--- packages/ui/src/button/button.tsx
+++ packages/ui/src/button/button.tsx
@@ -8,7 +8,7 @@ export function Button(p: ButtonProps) {
   return (
     <button
       {...p}
-      className={`bg-blue-500 hover:bg-blue-600 text-white font-medium py-2 px-4 rounded cursor-pointer transition-colors ${p.className || ''}`}
+      className={`bg-blue-400 hover:bg-blue-600 text-white font-medium py-2 px-4 rounded cursor-pointer transition-colors ${p.className || ''}`}
     >
       {p.children}
     </button>
